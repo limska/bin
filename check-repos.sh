f91b998889ac6d04b07f9d54098b8e3568c598dd
Sava Slijepcevic <sava.slijepcevic@cd-adapco.com>
2019-09-04 11:49:24 +0100

    Fix formating

--- check-repos.sh
+++ check-repos.sh
@@ -2,13 +2,19 @@
 
 CWD=`pwd`
 
+echo "Checking ~/bin"
 cd ~/bin
 git status
+echo
 
+echo "Checking ~/dotfiles"
 cd ~/dotfiles
 git status
+echo
 
+echo "Checking ~/dotfiles_local"
 cd ~/dotfiles_local
 git status
+echo
 
 cd $CWD
