32115a39cc60e825d8abeb486dbfc07b864c0dda
Sava Slijepcevic <sava.slijepcevic@cd-adapco.com>
2020-01-22 17:20:39 +0000

    Added cd to tmux-build.

--- tmux-build.sh
+++ tmux-build.sh
@@ -1,14 +1,17 @@
-#!/bin/bash
+#!/bin/zsh
 SESSION=build
+SRC="~/src/starccm/dev/star"
+
+cd $SRC
 
 tmux -2 new-session -d -s $SESSION -n 'git' 
 
 # Setup a window for tailing log files
-tmux new-window -t $SESSION:2 -n 'vim' -c ~/src/starccm/dev/star
-tmux new-window -t $SESSION:3 -n 'cmake' -c ~/src/starccm/dev/star
-tmux new-window -t $SESSION:4 -n 'java' -c ~/src/starccm/dev/star
-tmux new-window -t $SESSION:5 -n 'python' -c ~/src/starccm/dev/star
-tmux new-window -t $SESSION:6 -n 'rsync' -c ~/src/starccm/dev/star
+tmux new-window -t $SESSION:2 -n 'vim' -c $SRC
+tmux new-window -t $SESSION:3 -n 'cmake' -c $SRC
+tmux new-window -t $SESSION:4 -n 'java' -c $SRC
+tmux new-window -t $SESSION:5 -n 'python' -c $SRC
+tmux new-window -t $SESSION:6 -n 'rsync' -c $SRC
 
 # Attach to session
 tmux -2 attach-session -t $SESSION
