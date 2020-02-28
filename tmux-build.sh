baa7566fc21601724b2f382eb7ac6ac812c7d64c
Sava Slijepcevic <sava.slijepcevic@cd-adapco.com>
2020-02-28 13:26:27 +0000

    Fix start directory for tmux-build

--- tmux-build.sh
+++ tmux-build.sh
@@ -1,17 +1,15 @@
 #!/bin/zsh
 SESSION=build
-SRC="~/src/starccm/dev/star"
+SRC="/u/cd8rit/src/starccm/dev/star"
 
-cd $SRC
-
-tmux -2 new-session -d -s $SESSION -n 'git' 
+cd $SRC; tmux -2 new-session -d -s $SESSION -n 'git'
 
 # Setup a window for tailing log files
 tmux new-window -t $SESSION:2 -n 'vim' -c $SRC
 tmux new-window -t $SESSION:3 -n 'cmake' -c $SRC
 tmux new-window -t $SESSION:4 -n 'java' -c $SRC
 tmux new-window -t $SESSION:5 -n 'python' -c $SRC
-tmux new-window -t $SESSION:6 -n 'rsync' -c $SRC
+tmux new-window -t $SESSION:6 -n 'startest' -c "${SRC}test"
 
 # Attach to session
 tmux -2 attach-session -t $SESSION
