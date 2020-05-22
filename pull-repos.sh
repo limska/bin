4dbc16bbd23d25728933e8bb4f2f52e565866fa7
Sava Slijepcevic <sava.slijepcevic@cd-adapco.com>
2020-05-22 21:01:28 +0100

    added pull-repos script

---
+++ pull-repos.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/zsh
+
+CWD=`pwd`
+
+echo "Checking ~/bin"
+cd ~/bin
+git pull -p
+git status
+echo
+
+echo "Checking ~/dotfiles"
+cd ~/dotfiles
+git pull -p
+git status
+echo
+
+echo "Checking ~/dotfiles_local"
+cd ~/dotfiles_local
+git pull -p
+git status
+echo
+
+cd $CWD
