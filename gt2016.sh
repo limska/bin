b3cac117ae5266f4600ce4954f8add406557ba79
Sava Slijepčević <sava.slijepcevic@cd-adapco.com>
2020-05-27 19:48:10 +0100

    Use $HOME

--- gt2016.sh
+++ gt2016.sh
@@ -2,6 +2,6 @@
 
 unset _JAVA_OPTIONS
 export GTIVERBOSE=1
-. /users/lonli6009a/sava/software/GTI/v2016/gtenv.sh
+. $HOME/software/GTI/v2016/gtenv.sh
 export GTI_TEST_VERSION=2016
 echo "GTIHOME=$GTIHOME"
