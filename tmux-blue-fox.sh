1131f329ca9ac715f6def764c19ec95f3cccd9cc
Sava Slijepčević <6360269+limska@users.noreply.github.com>
2023-06-19 21:53:03 +0100

    Fix typo

--- tmux-blue-fox.sh
+++ tmux-blue-fox.sh
@@ -9,7 +9,7 @@ cd $SRC; tmux -2 new-session -d -s $SESSION -n 'src'
 tmux new-window -t $SESSION:2 -n 'amt-services' -c $SRC/amt-services
 tmux new-window -t $SESSION:3 -n 'blue-fox-services' -c $SRC/amt-services/blue-fox
 tmux new-window -t $SESSION:4 -n 'frontend' -c $SRC/frontend
-tmux new-window -t $SESSION:5 -n 'cutting-params-service' -c $REPOS/cutting-params-service/project/cutting-params-service
+tmux new-window -t $SESSION:5 -n 'cutting-params-service' -c $REPOS/cutting-params-service/projects/cutting-params-service
 tmux new-window -t $SESSION:6 -n 'cam-assist' -c $REPOS/engine
 
 # Attach to session
