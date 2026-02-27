6dda114c1a86276ce8da29d9650eb0cc2862ebbe
Sava Slijepčević <6360269+limska@users.noreply.github.com>
2026-02-27 15:01:42 +0100

    Added packageing

--- get_runner.sh
+++ get_runner.sh
@@ -15,6 +15,11 @@ curl -LsSf https://astral.sh/uv/install.sh | env UV_INSTALL_DIR="$(pwd)/bin" sh
 git clone git@github.com:StromungsRaum/runner.git
 git clone git@github.com:StromungsRaum/simodlib.git
 
+TARBALL="runner_$(date +'%Y_%m_%d-%H%M').tar.gz"
+tar czf $TARBALL runner
+echo
+echo "Packaged runner: $TARBALL"
+
 echo
 echo "Update PATH before using:"
 echo "export PATH=$(pwd)/bin:\$PATH"
@@ -22,3 +27,7 @@ echo
 echo "To test:"
 echo "$(pwd)/${TOP_DIR}/scripts/run --help"
 echo
+
+
+
+
