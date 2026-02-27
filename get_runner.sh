22e60b69525ded01be6cd7dae735024d67a8e11b
Sava Slijepčević <6360269+limska@users.noreply.github.com>
2026-02-27 14:31:29 +0100

    Remove trial run

--- get_runner.sh
+++ get_runner.sh
@@ -8,10 +8,6 @@ curl -LsSf https://astral.sh/uv/install.sh | env UV_INSTALL_DIR="$(pwd)/bin" sh
 git clone git@github.com:StromungsRaum/runner.git
 git clone git@github.com:StromungsRaum/simodlib.git
 
-export PATH=$(pwd)/bin:$PATH
-echo "$(pwd)/runner/scripts/run --help"
-$(pwd)/runner/scripts/run --help
-
 echo
 echo "Update PATH befor using:"
 echo "export PATH=$(pwd)/bin:\$PATH"
