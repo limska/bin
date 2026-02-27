e6a4b1071758aef14743e69c7c5074ce30d2e70c
Sava Slijepčević <6360269+limska@users.noreply.github.com>
2026-02-27 14:15:28 +0100

    Improve help

--- get_runner.sh
+++ get_runner.sh
@@ -5,13 +5,17 @@ cd runner
 
 curl -LsSf https://astral.sh/uv/install.sh | env UV_INSTALL_DIR="$(pwd)/bin" sh
 
-git clone git@github.com:StromungsRaum/runner.git 
-git clone git@github.com:StromungsRaum/simodlib.git 
-
-echo
-echo "export PATH=$(pwd)/bin:\$PATH"
-echo
+git clone git@github.com:StromungsRaum/runner.git
+git clone git@github.com:StromungsRaum/simodlib.git
 
 export PATH=$(pwd)/bin:$PATH
 echo "$(pwd)/runner/scripts/run --help"
 $(pwd)/runner/scripts/run --help
+
+echo
+echo "Update PATH befor using:"
+echo "export PATH=$(pwd)/bin:\$PATH"
+echo
+echo "To test:"
+echo "$(pwd)/runner/scripts/run --help"
+echo
